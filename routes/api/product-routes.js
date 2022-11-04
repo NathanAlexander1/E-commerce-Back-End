c61510eade1236578f689648174259bd07ee8fcb
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-11-03 23:32:59 -0400

    product routes working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -127,8 +127,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!product) {
+      return res.status(400).json({ message: "No product!" });
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
